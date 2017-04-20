e268bc2f632e9c4799a0b2e29df682e0f781c80d
enrique.otero <enrique.otero@beeva.com>
2017-04-20 10:34:49 +0200

    Fix trigger name in mycaller.sh

--- mycaller.sh
+++ mycaller.sh
@@ -3,7 +3,7 @@
 node_process_id=$(ps ax | grep mytrigger.sh | grep -v grep | cut -d ' ' -f1)
 echo $node_process_id
 if [[ -z $node_process_id ]]; then
-  ./trigger.sh
+  ./mytrigger.sh
 else
   echo "mytrigger.sh is running..."
 fi
