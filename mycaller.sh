47d43ba310c72d2766c8a465cbc31be952d0cb48
Enrique Otero <enrique.otero@beeva.com>
2017-04-21 09:30:34 +0200

    Fix pid detection

--- mycaller.sh
+++ mycaller.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-node_process_id=$(ps ax | grep mytrigger.sh | grep -v grep | cut -d ' ' -f1)
+node_process_id=$(ps aux | grep ./mytrigger | grep -v grep | awk '{print $2}')
 echo $node_process_id
 if [[ -z $node_process_id ]]; then
   ./mytrigger.sh
